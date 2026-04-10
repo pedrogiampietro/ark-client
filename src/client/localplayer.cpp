e7f19c6886672e3fd82283872c9e433bca87644d
Pedro Giampietro <pedrogiampietro@hotmail.com>
2026-04-10 22:34:41 +0000

    walking

--- src/client/localplayer.cpp
+++ src/client/localplayer.cpp
@@ -139,6 +139,7 @@ void LocalPlayer::walk(const Position& oldPos, const Position& newPos)
 
         // invalid pre walk
         m_preWalking.clear();
+        m_stepUsesPrewalkOffset = false;
         m_serverWalking = true;
         if (m_serverWalkEndEvent)
             m_serverWalkEndEvent->cancel();
@@ -150,6 +151,7 @@ void LocalPlayer::walk(const Position& oldPos, const Position& newPos)
         }
 
         m_serverWalking = true;
+        m_stepUsesPrewalkOffset = false;
         if (m_serverWalkEndEvent)
             m_serverWalkEndEvent->cancel();
         m_lastAutoWalkRetries = 0;
@@ -169,6 +171,7 @@ void LocalPlayer::preWalk(Otc::Direction direction)
         m_serverWalkEndEvent->cancel();
 
     m_lastPrewalkDone = false;
+    m_stepUsesPrewalkOffset = true;
     m_preWalking.push_back(newPos);
     if (m_preWalking.size() > 1)
         g_map.requestVisibleTilesCacheUpdate();
@@ -347,7 +350,7 @@ void LocalPlayer::stopWalk() {
 void LocalPlayer::updateWalkOffset(uint8 totalPixelsWalked, bool inNextFrame)
 {
     // pre walks offsets are calculated in the oposite direction
-    if(isPreWalking()) {
+    if(m_stepUsesPrewalkOffset) {
         Point& walkOffset = inNextFrame ? m_walkOffsetInNextFrame : m_walkOffset;
         walkOffset = Point(0,0);
         if(m_walkDirection == Otc::North || m_walkDirection == Otc::NorthEast || m_walkDirection == Otc::NorthWest)
@@ -388,6 +391,7 @@ void LocalPlayer::terminateWalk()
     Creature::terminateWalk();
     m_idleTimer.restart();
     m_preWalking.clear();
+    m_stepUsesPrewalkOffset = false;
     m_walking = false;
 
     if(m_serverWalking) {
--- src/client/localplayer.h
+++ src/client/localplayer.h
@@ -168,6 +168,7 @@ private:
     std::list<Position> m_preWalking;
     bool m_serverWalking = false;
     bool m_lastPrewalkDone = false;
+    bool m_stepUsesPrewalkOffset = false;
     WalkMatrix m_walkMatrix;
 
     bool m_premium = false;
